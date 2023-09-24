54170bd7363552cc8b0ed3a41837c682ffcb6036
Robertw8 <oniedgaf@gmail.com>
2023-09-25 02:31:20 +0300

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import reactRefresh from "@vitejs/plugin-react-refresh";
 
 export default defineConfig({
 	plugins: [react()],
-	base: "/goit-react-hw-05-movies/",
 });
