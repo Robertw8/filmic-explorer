38d3e85dc36ad8c20a3d1dfbfd0e659f96af6c21
Robertw8 <oniedgaf@gmail.com>
2023-09-25 03:17:47 +0300

    NotFound demo

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const NotFound = () => {
+	return (
+		<div>
+			404 404 404 <Link to={"/"}>BACK TO HOMEPAGE</Link>
+		</div>
+	);
+};
+
+export default NotFound;
