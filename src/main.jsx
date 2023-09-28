ed2388da4e2de6a6ba99a8c1e1039e11c42c391a
Robertw8 <oniedgaf@gmail.com>
2023-09-28 03:10:56 +0300

    refactored main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { App } from "./components/App/App";
-// import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import App from "./components/App/App";
 import { BrowserRouter } from "react-router-dom";
+import { Global, ThemeProvider } from "@emotion/react";
+import theme from "./styles/theme";
+import { globalStyles } from "./styles/globalStyles";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<BrowserRouter basename='/goit-react-hw-05-movies'>
-			<App />
+		<BrowserRouter basename='/goit-react-hw-05-movies/'>
+			<ThemeProvider theme={theme}>
+				<Global styles={globalStyles} />
+				<App />
+			</ThemeProvider>
 		</BrowserRouter>
 	</React.StrictMode>,
 );
