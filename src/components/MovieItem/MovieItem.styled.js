e36efcd28570e4e5fddbf5aee042989dba2e3160
Robertw8 <oniedgaf@gmail.com>
2023-10-01 22:30:28 +0300

    fix movies images sizing

--- src/components/MovieItem/MovieItem.styled.js
+++ src/components/MovieItem/MovieItem.styled.js
@@ -3,20 +3,20 @@ import { Card } from "@mui/material";
 import CardMedia from "@mui/material/CardMedia";
 
 const StyledCard = styled(Card)`
-	background-color: #4d0dd9;
-	border-radius: 10px;
+  background-color: #4d0dd9;
+  border-radius: 10px;
 `;
 
 const CardTitle = styled.h3`
-	font-size: 18px;
-	text-align: center;
-	font-family: "Roboto", sans-serif;
-	color: #eee7fe;
+  font-size: 18px;
+  text-align: center;
+  font-family: "Roboto", sans-serif;
+  color: #eee7fe;
 `;
 
 const StyledCardMedia = styled(CardMedia)`
-	width: 500px;
-	height: 300px;
+  width: 500px;
+  height: 700px;
 `;
 
 export { StyledCard, CardTitle, StyledCardMedia };
