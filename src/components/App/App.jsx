21ffad4240db39703f6fb6a58dc9c78b9ab8ba0c
Robertw8 <oniedgaf@gmail.com>
2023-10-01 20:59:30 +0300

    move Suspense to /SharedLayout

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,30 +1,30 @@
-import React, { lazy, Suspense } from "react";
+import React, { lazy } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import SharedLayout from "../SharedLayout/SharedLayout";
 import Loader from "../Loader/Loader";
 
 const Home = lazy(() => import("../../pages/Home"));
 const Movies = lazy(() => import("../../pages/Movies"));
-const MovieDetails = lazy(() => import("../../pages/MovieDetails/MovieDetails"));
+const MovieDetails = lazy(() =>
+  import("../../pages/MovieDetails/MovieDetails")
+);
 const Cast = lazy(() => import("../Cast/Cast"));
 const Reviews = lazy(() => import("../Reviews/Reviews"));
 
 const App = () => {
-	return (
-		<Suspense fallback={<Loader top='50%' />}>
-			<Routes>
-				<Route path='/' element={<SharedLayout />}>
-					<Route index element={<Home />} />
-					<Route path='movies' element={<Movies />} />
-					<Route path='movies/:movieId/*' element={<MovieDetails />}>
-						<Route path='cast' element={<Cast />} />
-						<Route path='reviews' element={<Reviews />} />
-					</Route>
-					<Route path='*' element={<Navigate to='/' />} />
-				</Route>
-			</Routes>
-		</Suspense>
-	);
+  return (
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<Home />} />
+        <Route path="movies" element={<Movies />} />
+        <Route path="movies/:movieId/*" element={<MovieDetails />}>
+          <Route path="cast" element={<Cast />} />
+          <Route path="reviews" element={<Reviews />} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,22 +1,25 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Outlet } from "react-router-dom";
 import { Container } from "../../styles/globalStyles";
 import Header from "../Header/Header";
 import NavBar from "../NavBar/NavBar";
+import Loader from "../Loader/Loader";
 
 const SharedLayout = () => {
-	return (
-		<>
-			<Container>
-				<Header>
-					<NavBar />
-				</Header>
-			</Container>
-			<Container>
-				<Outlet />
-			</Container>
-		</>
-	);
+  return (
+    <>
+      <Container>
+        <Header>
+          <NavBar />
+        </Header>
+      </Container>
+      <Suspense fallback={<Loader top="50%" />}>
+        <Container>
+          <Outlet />
+        </Container>
+      </Suspense>
+    </>
+  );
 };
 
 export default SharedLayout;
