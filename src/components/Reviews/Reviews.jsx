8799234d9bf10cc2f498fd83c15b24861a768272
Robertw8 <oniedgaf@gmail.com>
2023-10-01 23:14:08 +0300

    refactored Reviews

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,45 +1,57 @@
 import React, { useState, useEffect } from "react";
+import {
+  CardsWrapper,
+  ReviewCard,
+  ReviewAuthor,
+  ReviewText,
+  ReviewLink,
+  NoReviews,
+} from "./Reviews.styled";
+import { CardContent } from "@mui/material";
+
 import getMovieReviews from "../../api/getMovieReviews";
 import { useParams } from "react-router-dom";
-import { CardsWrapper, ReviewCard, ReviewAuthor, ReviewText, ReviewLink, NoReviews } from "./Reviews.styled";
-import { CardContent } from "@mui/material";
 import formatDate from "../../helpers/formatDate";
 
 const Reviews = () => {
-	const [reviews, setReviews] = useState([]);
-	const { movieId } = useParams();
+  const [reviews, setReviews] = useState([]);
+  const { movieId } = useParams();
 
-	useEffect(() => {
-		const fetchMovieReviews = async () => {
-			const response = await getMovieReviews(movieId);
-			setReviews(response.results);
-		};
+  useEffect(() => {
+    const fetchMovieReviews = async () => {
+      const response = await getMovieReviews(movieId);
+      setReviews(response.results);
+    };
 
-		fetchMovieReviews();
-	}, [movieId]);
+    fetchMovieReviews();
+  }, [movieId]);
 
-	return (
-		<CardsWrapper>
-			{reviews.length ? (
-				reviews.map(({ id, author, content, created_at, url }) => (
-					<ReviewCard key={id} elevation={10}>
-						<CardContent>
-							<ReviewAuthor>
-								{author} on {formatDate(created_at)}
-							</ReviewAuthor>
-							<ReviewText>{content}</ReviewText>
+  return (
+    <CardsWrapper>
+      {reviews.length ? (
+        reviews.map(({ id, author, content, created_at, url }) => (
+          <ReviewCard key={id} elevation={10}>
+            <CardContent>
+              <ReviewAuthor>
+                {author} on {formatDate(created_at)}
+              </ReviewAuthor>
+              <ReviewText>{content}</ReviewText>
 
-							<ReviewLink href={url} target='_blank' rel='noopener norefferer nofollow'>
-								{url}
-							</ReviewLink>
-						</CardContent>
-					</ReviewCard>
-				))
-			) : (
-				<NoReviews>No reviews given.</NoReviews>
-			)}
-		</CardsWrapper>
-	);
+              <ReviewLink
+                href={url}
+                target="_blank"
+                rel="noopener norefferer nofollow"
+              >
+                {url}
+              </ReviewLink>
+            </CardContent>
+          </ReviewCard>
+        ))
+      ) : (
+        <NoReviews>No reviews given.</NoReviews>
+      )}
+    </CardsWrapper>
+  );
 };
 
 export default Reviews;
