27962145810aefbb70653d99f0894c66d7e85ad2
Robertw8 <oniedgaf@gmail.com>
2023-10-01 19:02:44 +0300

    remove NotFound

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,10 @@
 import React, { lazy, Suspense } from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import SharedLayout from "../SharedLayout/SharedLayout";
 import Loader from "../Loader/Loader";
 
 const Home = lazy(() => import("../../pages/Home"));
 const Movies = lazy(() => import("../../pages/Movies"));
-const NotFound = lazy(() => import("../../pages/NotFound"));
 const MovieDetails = lazy(() => import("../../pages/MovieDetails/MovieDetails"));
 const Cast = lazy(() => import("../Cast/Cast"));
 const Reviews = lazy(() => import("../Reviews/Reviews"));
@@ -21,7 +20,7 @@ const App = () => {
 						<Route path='cast' element={<Cast />} />
 						<Route path='reviews' element={<Reviews />} />
 					</Route>
-					<Route path='*' element={<NotFound />} />
+					<Route path='*' element={<Navigate to='/' />} />
 				</Route>
 			</Routes>
 		</Suspense>
