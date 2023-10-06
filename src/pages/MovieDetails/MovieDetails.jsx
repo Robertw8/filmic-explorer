b8b8703f782dd1ab4d63c3f6ee731b7f60c4fbc5
Robertw8 <oniedgaf@gmail.com>
2023-10-06 21:26:12 +0300

    change Loader positioning

--- src/pages/MovieDetails/MovieDetails.jsx
+++ src/pages/MovieDetails/MovieDetails.jsx
@@ -61,7 +61,7 @@ const MovieDetails = () => {
               <MovieLinks />
             </MovieContent>
           </MovieDetailsWrapper>
-          <Suspense fallback={<Loader top="70%" />}>
+          <Suspense fallback={<Loader top="100%" />}>
             <Container>
               <Outlet />
             </Container>
